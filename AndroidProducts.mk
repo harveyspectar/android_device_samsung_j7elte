5aa87263b46eb382646b7e3223be5aac5184b606
harveyspectar <ultimatepravin72@gmail.com>
2020-08-12 10:08:36 +0000

    j7elte: Hello RR!

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 LOCAL_PATH := device/samsung/j7elte
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_PATH)/lineage_j7elte.mk
+    $(LOCAL_PATH)/rr_j7elte.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_j7elte-user \
-    lineage_j7elte-userdebug \
-    lineage_j7elte-eng
+    rr_j7elte-user \
+    rr_j7elte-userdebug \
+    rr_j7elte-eng
